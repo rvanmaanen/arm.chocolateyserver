8191e1e231bae9c1db55cc25d34ecfee0aac9b2a
Reinier van Maanen <rvanmaanen@xpirit.com>
2018-07-04 17:12:34 +0200

    Moved todo's and stuff to readme

--- ChocolateyServer/ChocolateyServer.ps1
+++ ChocolateyServer/ChocolateyServer.ps1
@@ -1,11 +1,3 @@
-#todo: finish this file, maybe split the DSC and the rest of the commands and make sure both are triggered from the arm template (see next todo).
-#todo: fix the modulesUrl in the arm template and check out the uploadartifacts funcationality in the deploy.ps1 file, maybe we can use that?
-#todo: remove the windowsosversion parameter from the arm file and hardcode it in the variables section
-#todo: clean up naming for all the resources created: MyVmNic -> ChocolateServerVmNic, etc.
-
-#tips: https://blogs.technet.microsoft.com/paulomarques/2016/07/06/azure-deployments-speeding-up-powershell-dsc-by-testing-locally-before-using-it-in-an-azure-resource-manager-template-or-from-a-powershell-deployment-script/
-#manual for choco server: https://chocolatey.org/docs/how-to-set-up-chocolatey-server#requirements
-
 Configuration ChocolateyServer
 {
     Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
