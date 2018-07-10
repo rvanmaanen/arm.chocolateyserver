015364cf319bc3c37253235f9efbe05855ccf7fc
Reinier van Maanen <rvanmaanen@xpirit.com>
2018-07-10 14:45:23 +0200

    Fixed issue 1 by cleaning up the double quotes: https://github.com/rvanmaanen/arm.chocolateyserver/issues/1

--- ChocolateyServer/ChocolateyServer.ps1
+++ ChocolateyServer/ChocolateyServer.ps1
@@ -4,14 +4,14 @@ Configuration ChocolateyServer
 
     WindowsFeature IIS
     {
-        Ensure = “Present”
-        Name = “Web-Server”
+        Ensure = "Present"
+        Name = "Web-Server"
     }
 
     WindowsFeature ASP
     {
-        Ensure = “Present”
-        Name = “Web-Asp-Net45”
+        Ensure = "Present"
+        Name = "Web-Asp-Net45"
     }
 
     WindowsFeature WebServerManagementConsole
